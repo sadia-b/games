bd0e51002f9527126cfe080a611d298902ec31a6
Sadia Bahadoor <sadia.bahadoor@gmail.com>
2025-04-08 15:44:24 -0400

    game logic completed

--- dice.py
+++ dice.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import random
+
+while True:
+  roll = input("Roll the dice? (y/n): ").lower()
+  if roll == 'y':
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    print(f'({dice1}, {dice2})')
+  elif roll == 'n':
+    print("Thanks for playing!")
+    break
+  else:
+    print('Invalid choice!')
